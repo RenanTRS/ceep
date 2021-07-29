cd3883d2ae4f491db81b4de3ee560678a7d3aa0c
Renan Souza <renantrsouza@gmail.com>
2021-07-29 11:58:35 -0400

    add tarefas quando clicar no botao

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,7 +1,16 @@
 function teste(evento){
     evento.preventDefault();
+
     const input = document.querySelector('.form-input').value;
-    console.log(input);
+    const ul = document.querySelector('[data-list]');
+
+    const li = document.createElement('li');
+    li.classList.add('task');
+
+    const conteudo = `<p class="content">${input}</p>`;
+    li.insertAdjacentHTML('beforeend', conteudo);
+
+    ul.appendChild(li);
 }
 
 const novaTarefa = document.querySelector('.form-button');
