6719f0c058915a3bf9f380bb06252a083fd4855c
Renan Souza <renantrsouza@gmail.com>
2021-07-30 18:27:24 -0400

    add função para riscar tarefas concluídas

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -22,9 +22,14 @@ function BotaoConclui(){
     const botaoConclui = document.createElement('button');
     botaoConclui.innerText = 'concluir';
     botaoConclui.classList.add('check-button');
-    botaoConclui.addEventListener('click', () => {
-        console.log('Fui clicado');
-    });
+    botaoConclui.addEventListener('click', concluirTarefa);
 
     return botaoConclui;
+}
+
+function concluirTarefa(evento){
+    const botaoConclui = evento.target;
+
+    const tarefaCompleta = botaoConclui.parentElement;
+    tarefaCompleta.classList.toggle('done');
 }
