fa23a0ad42bc6e345e64343d44b11036d5513440
Renan Souza <renantrsouza@gmail.com>
2021-08-05 14:03:42 -0400

    add novas funções

--- js/listaDeTarefas.js
+++ js/listaDeTarefas.js
@@ -1,17 +1,23 @@
 import { BotaoConclui } from "./btn-conclui.js";
 import { BotaoDeleta } from "./btn-deletar.js";
 
-function teste(evento){
-    evento.preventDefault();
-
+function novoItem(event){
+    event.preventDefault();
+    const ul = document.querySelector('[data-list]');
     const input = document.querySelector('.form-input').value;
-    
     const dateValue = document.querySelector('[data-form-date]').value;
     const date = moment(dateValue);
     const dateFormat = date.format('DD/MM/YYYY');
-    
-    const ul = document.querySelector('[data-list]');
 
+    const dados = {
+        input,
+        dateFormat
+    };
+    //criaTarefa(dados);
+    ul.appendChild(criaTarefa(dados));
+}
+
+function criaTarefa({input, dateFormat}){
     const li = document.createElement('li');
     li.classList.add('task');
 
@@ -20,9 +26,9 @@ function teste(evento){
 
     li.appendChild(BotaoConclui());
     li.appendChild(BotaoDeleta());
-    ul.appendChild(li);
+    return li;
 }
 
 //const novaTarefa = document.querySelector('.form-button').addEventListener('click', teste);
 const novaTarefa = document.querySelector('.form-button');
-novaTarefa.addEventListener('click', teste);
+novaTarefa.addEventListener('click', novoItem);
